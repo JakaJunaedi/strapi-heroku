7190d9b8393da0cca9e5ac85d3476de30d125934
jaka junaedi <jaka.junaedi03@gmail.com>
2021-12-08 10:00:19 +0700

    update database production

--- config/database.js
+++ config/database.js
@@ -1,13 +1,39 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const parse = require("pg-connection-string").parse;
+module.exports = ({ env }) => {
+  if (env("NODE_PRODUCTION") === "production") {
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.username,
+            password: config.password
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi-heroku'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'password051455'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi-heroku"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "password051455"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
-  },
-});
+  };
+};
